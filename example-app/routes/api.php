845037ae08f12766d1a749413049d8dcd1868165
Gabriel Vieira <gabriel.vieira@irroba.com.br>
2022-09-07 08:34:45 -0300

    shell python && alter dockerfile

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
+use robertogallea\LaravelPython\Services\LaravelPython;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +21,19 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/teste', function (Request $request) {
+    $a = shell_exec("python3 /var/www/html/test.py");
+    echo '<pre>' , var_dump($a) , '</pre>'; die;
+    $process = new Process(["python3 /var/www/html/test.py"]);
+    $process->run();
+
+    if (!$process->isSuccessful()) {
+        throw new ProcessFailedException($process);
+    }
+
+    $data = $process->getOutput();
+
+    dd($data);
+});
+
